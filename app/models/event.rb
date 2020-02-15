82408f897ba2dd302e87d94fa8e53f351838617d
beckyphan <beckyphan@live.com>
2020-02-15 15:34:47 -0800

    updated event model with associations, clarified class_name and foreign_key user_id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :movie
-  belongs_to :host
+  belongs_to :host, class_name: "User", foreign_key: "user_id"
+
+  has_many :comments
+  has_many :attendees, class_name: "User", foreign_key: "user_id"
 end
