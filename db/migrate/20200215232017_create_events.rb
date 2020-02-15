3c18800fc4bb68f51168d68722c4a08892aa5f56
beckyphan <beckyphan@live.com>
2020-02-15 15:20:45 -0800

    created event using resources generator

---
+++ db/migrate/20200215232017_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.string :event_title
+      t.datetime :date_time
+      t.belongs_to :movie, null: false, foreign_key: true
+      t.belongs_to :host, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
