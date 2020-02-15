10da1e7f00fb4d8162b080cf7772d1c58a9a2f6d
beckyphan <beckyphan@live.com>
2020-02-15 15:28:59 -0800

    updated user model with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_many :reviews
+
+  has_many :hosted_events, class_name: "Event"
+  has_many :attended_events, class_name: "Event"
+
+  has_many :movies, through: :reviews
+  has_many :comments, through: :events
 end
