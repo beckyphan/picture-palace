3199c51f0681005c7dd0c8e2f726e1d7b40a0146
beckyphan <beckyphan@live.com>
2020-02-15 15:22:48 -0800

    created review using resource generator'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reviews
   resources :events
   resources :movies
   resources :users
---
+++ db/migrate/20200215232225_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.string :review_title
+      t.text :description
+      t.integer :rating
+      t.belongs_to :movie, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
