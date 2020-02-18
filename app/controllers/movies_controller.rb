1bbe0a17ae6a9e50f9c2630e7215e24058237139
beckyphan <beckyphan@live.com>
2020-02-18 15:48:44 -0800

    minor update to view - application layout, added actions for movies controller"

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,27 @@
 class MoviesController < ApplicationController
 
   def landing
-  end 
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
