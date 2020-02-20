dbd331540f59adb8bd8a7f37b4661deef01e3824
beckyphan <beckyphan@live.com>
2020-02-20 14:24:35 -0800

    added event validations to model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  validates_presence_of :event_title, :date_time, :movie_id
+  validates :host_id, uniqueness: {scope: :date_time, message: ". You already hosting an event at that date/time."}
+
   belongs_to :movie
   belongs_to :host, class_name: "User", foreign_key: "host_id"
 
