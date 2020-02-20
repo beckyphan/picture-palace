cea78a5261d59f68b834a2f55a3c3095a5528eb8
beckyphan <beckyphan@live.com>
2020-02-20 13:00:59 -0800

    updated new movie form on movies#index, added/styled error message in movie helper/css, corrected formatting of user and movie forms

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,11 @@
 module MoviesHelper
+
+  def new_movie_errors(movie)
+    message = ""
+    @movie.errors.full_messages.each do |m|
+      message += "<li class='error'> #{m} </li>"
+    end
+    return message
+  end
+
 end
