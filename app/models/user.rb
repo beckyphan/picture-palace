2ab59786bb6031943f839f22f0045fae6490a6bf
beckyphan <beckyphan@live.com>
2020-02-20 17:48:42 -0800

    corrected join table attendees_events

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :reviews
 
   has_many :hosted_events, class_name: "Event", foreign_key: 'host_id'
-  has_many :attended_events, through: :guestlist, foreign_key: 'attendee_id', source: :event
+  has_many :attended_events, through: :attendees_events, foreign_key: 'attendee_id', source: :event
 
   has_many :movies, through: :reviews
   has_many :comments, through: :events
