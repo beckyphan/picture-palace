cad5575004c9cf308b198d2c643eaf5caa36c23d
beckyphan <beckyphan@live.com>
2020-02-25 12:24:10 -0800

    updated application controller with helper methods current_user, logged_in?, authenticate_user!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?, :authenticate_user!
 
   private
 
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 
+  def logged_in?
+    !!current_user
+  end
+
+  def authenticate_user!
+    redirect_to login_path if !logged_in?
+  end
 end
