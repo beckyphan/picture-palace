e3336c6d235d7b7520e135303ee034e76aa8658d
beckyphan <beckyphan@live.com>
2020-02-27 23:22:54 -0800

    create comment with params and redirect to event_path(@event)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,12 @@ class CommentsController < ApplicationController
     end
 
     def create
+      @comment = Comment.new(note: params[:comment][:note], event_id: params[:event_id], user_id: current_user.id)
+      @event = Event.find_by_id(params[:event_id])
+
+      @comment.save
+
+      redirect_to event_path(@event)
     end
 
     def new
