6a8d4482fc13d226133ba986e6d8e4e2fc715fee
beckyphan <github email address>
2020-03-08 15:21:43 -0700

    edited flash messages for guestlist actions on event#show

--- app/controllers/guestlists_controller.rb
+++ app/controllers/guestlists_controller.rb
@@ -2,15 +2,14 @@ class GuestlistsController < ApplicationController
 
   def create
     @attendees_event = Guestlist.create(event_id: params[:event_id], attendee_id: current_user.id, status: true)
-    flash.now[:notice] = "Your RSVP has been recorded."
+    flash[:notice] = "Your RSVP has been recorded."
     redirect_to event_path(params[:event_id])
   end
 
   def update
-    binding.pry
     @attendees_event = Guestlist.find_by(event_id: params[:event_id], attendee_id: current_user.id)
     @attendees_event.destroy
-    flash.now[:notice] = "Your RSVP has been updated."
+    flash[:notice] = "Your RSVP has been updated."
     redirect_to event_path(params[:event_id])
   end
 
