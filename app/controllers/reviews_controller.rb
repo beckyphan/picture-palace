40e43fe856d60428599ce20d8d95be0e8bd2ae85
beckyphan <github email address>
2020-03-12 19:33:44 -0700

    users can write a review on movie#show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,6 +4,17 @@ class ReviewsController < ApplicationController
   end
 
   def create
+     @review = Review.new(review_params)
+     @review.movie_id = params[:movie_id]
+     @review.user_id = current_user.id
+
+     if @review.save
+       flash[:notice] = "Thanks for sharing a review!"
+       redirect_to movie_path(params[:movie_id])
+     else
+       flash.now[:alert] = "Errors on page. Please correct:"
+       render 'new'
+     end
   end
 
   def new
@@ -20,5 +31,10 @@ class ReviewsController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+  def review_params
+    params.require(:review).permit(:review_title, :rating, :description)
+  end
+
 end
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+
+  def new_review_errors(review)
+    message = ""
+    @review.errors.full_messages.each do |m|
+      message += "<li class='error'> #{m} </li>"
+    end
+    return message
+  end
+
 end
