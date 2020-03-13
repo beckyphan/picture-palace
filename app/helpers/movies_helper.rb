c18b2ae06eb827376ffc74667780e7a9432fcc28
beckyphan <github email address>
2020-03-12 20:22:39 -0700

    updated errors helper method, generalized to object_errors

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,13 +1,5 @@
 module MoviesHelper
 
-  def new_movie_errors(movie)
-    message = ""
-    @movie.errors.full_messages.each do |m|
-      message += "<li class='error'> #{m} </li>"
-    end
-    return message.html_safe
-  end
-
   def movie_rating_stars(num)
     message = ""
     num.times {message += "<img src='https://upload.wikimedia.org/wikipedia/commons/2/29/Gold_Star.svg' width='18px'>"}
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,11 +1,3 @@
 module ReviewsHelper
 
-  def new_review_errors(review)
-    message = ""
-    @review.errors.full_messages.each do |m|
-      message += "<li class='error'> #{m} </li>"
-    end
-    return message.html_safe
-  end
-
 end
