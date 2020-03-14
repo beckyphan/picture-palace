2d4a38ed82e891ad696a6efce1b2c93f2f2a12b8
beckyphan <github email address>
2020-03-13 21:22:03 -0700

    created scope for Guestlist to pull lists for specific attendee. event_instance.attendees will list attendees of the event while Guestlist.attendee(user) requires an argument and pulls the guestlists from which a specific user has rsvped

--- app/models/guestlist.rb
+++ app/models/guestlist.rb
@@ -4,6 +4,7 @@ class Guestlist < ApplicationRecord
 
   scope :event_guest_counts, -> { self.group(:event_id).count }
   scope :order_events_by_popularity, -> { self.group(:event_id).order("count(*)").count }
+  scope :attendee, -> (attendee_object) { where('attendee_id = ?', attendee_object.id)}
 
   def self.most_popular_event
     event_id = self.order_events_by_popularity.first.first
