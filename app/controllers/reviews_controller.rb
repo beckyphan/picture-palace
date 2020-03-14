3bc6be01f61a3364a1ce1716babb49db96b1d863
beckyphan <github email address>
2020-03-13 20:19:02 -0700

    created link button on review#index view to add a new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
   def index
     @movie = Movie.find_by(id: params[:movie_id])
     @reviews = Review.where(movie_id: params[:movie_id])
+    @review = Review.new
   end
 
   def create
