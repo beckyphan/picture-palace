04946f366fbbca32efda305c273c62e15033c456
beckyphan <github email address>
2020-03-13 19:13:49 -0700

    created scopes for Review to group by movie, order by most reviewed, and class method of most_reviewed_movie

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :movie
   belongs_to :user
+
+  scope :movie_review_counts, -> { self.self.group(:movie_id).count}
+  scope :order_by_movie_with_most_reviews, -> { self.group(:movie_id).order("count(*)").count }
+
+  def self.most_reviewed_movie
+    movie_id = self.order_by_movie_with_most_reviews.first.first
+    @movie = Movie.find_by(id: movie_id)
+  end
 end
