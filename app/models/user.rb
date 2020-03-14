23e5a074db2a329fcd71875ecbd69519f7b404ba
beckyphan <github email address>
2020-03-13 21:19:14 -0700

    corrected comment re: aliasing

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
 
   has_many :reviews
 
-  has_many :events, foreign_key: 'host_id' # host.events
-  # foreign_key is referencing what column to look for in the User table
+  has_many :events, foreign_key: 'host_id' # event.host => user
+  # foreign_key is referencing what column to look for in the Event table
 
   has_many :guestlists
-  has_many :events, through: :guestlists # user.events
+  has_many :events, through: :guestlists, foreign_key: 'attendee_id' #guestlist.attendee => user
   # foreign_key is referencing what column to look for in the Guestlist table
 
   has_many :movies, through: :reviews
