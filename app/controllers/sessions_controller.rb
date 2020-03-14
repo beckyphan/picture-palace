5208480d6c5ebcb06cb9c1803de5567e956bc3a9
beckyphan <github email address>
2020-03-14 01:26:55 -0700

    created oauth login method to find_or_create through google sign-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,10 @@ class SessionsController < ApplicationController
   def login
     @user = User.find_by(email: params[:user][:email])
 
-    if @user && @user.authenticate(params[:user][:password])
+    if @user && @user.omniauth
+      flash.now[:alert] = "Please sign-in using Google."
+      render 'sessions/new'
+    elsif @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     elsif @user
@@ -15,6 +18,25 @@ class SessionsController < ApplicationController
       render 'sessions/new'
     else
       flash.now[:alert] = "Email not found. Please try again."
+      render 'sessions/new'
+    end
+  end
+
+  def oauth_login
+    user_info = request.env['omniauth.auth']
+
+    @user = User.find_or_create_by(email: user_info['info']['email']) do |u|
+      u.name = user_info['info']['name']
+      u.username = user_info['info']['name'].gsub(/\s+/, "")
+      u.password = ENV['DEFAULT_PASSWORD']
+      u.omniauth = true
+    end
+
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash.now[:alert] = "Please try again."
       render 'sessions/new'
     end
   end
