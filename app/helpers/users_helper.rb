4b0bea7021c74657ad46140ede5676c497936cac
beckyphan <github email address>
2020-03-16 20:32:59 -0700

    updated google log-in/sign-up since there are the same

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,3 @@
 module UsersHelper
 
-  def registration_errors(user)
-    message = ""
-    @user.errors.full_messages.each do |m|
-      message += "<li class='error'> #{m} </li>"
-    end
-    return message
-  end
-
 end
