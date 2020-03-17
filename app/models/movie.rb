9ba0f232c13eb0502a436f9604776e349c10e09f
beckyphan <beckyphan@live.com>
2020-03-17 14:17:21 -0700

    aliased user to be called viewer in Review model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,14 @@ class Movie < ApplicationRecord
   has_many :reviews
   has_many :events
 
+
+  has_many :viewers, through: :reviews, source: :viewer, foreign_key: 'user_id'
+  # has_many :users, through: :reviews
+  # has_many :viewers, through :reviews ... so make sure Review table uses same name to reference user & indicates the table (User) and foreign_key (user_id)
+  # source: :viewer indicates who the association belongs_to (User)
+  # foreign_key indicates which column to look in for the Viewer (User) table
+  # ex. SELECT "users".* FROM "users" INNER JOIN "reviews" ON "users"."id" = "reviews"."user_id" WHERE "reviews"."movie_id" = $1 LIMIT $2  [["movie_id", 12], ["LIMIT", 11]]
+
   def avg_rating
     self.reviews.average("rating").round
   end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :movie
-  belongs_to :user
+  belongs_to :viewer, class_name: "User", foreign_key: "user_id"
+  # belongs_to :user, class_name: "User", foreign_key: "user_id"
+  # means Review.user looks up a a user by user_id in the User table
+  # aka Review.viewer looks up a viewer by user_id in the User table
 
   validates_presence_of :review_title, :rating
   validates :user_id, uniqueness: {scope: :movie_id, message: "has already written a review for this movie."}
