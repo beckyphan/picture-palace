cb29ea85c1af4271b5a76620f2fbab0ac7ff4b3e
beckyphan <beckyphan@live.com>
2020-03-17 15:35:47 -0700

    ensured dependencies so if user is destroyed, events hosted and attending as well as comments and reviews will be deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   validates_presence_of :name, :username, :email
   validates_uniqueness_of :username, :email
 
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
-  has_many :events, foreign_key: 'host_id' # event.host => user
+  has_many :events, foreign_key: 'host_id', dependent: :destroy # event.host => user
   # foreign_key is referencing what column to look for in the Event table
 
-  has_many :guestlists
+  has_many :guestlists, foreign_key: 'attendee_id', dependent: :destroy
   has_many :events, through: :guestlists, foreign_key: 'attendee_id' #guestlist.attendee => user
   # foreign_key is referencing what column to look for in the Guestlist table
 
   has_many :movies, through: :reviews
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
 end
