32e4fda1c4c4cd59d953f9bf1d294fbd61a43e3d
beckyphan <github email address>
2020-03-16 21:14:49 -0700

    cleaned up guestlist controller/no longer using status

--- app/controllers/guestlists_controller.rb
+++ app/controllers/guestlists_controller.rb
@@ -1,7 +1,7 @@
 class GuestlistsController < ApplicationController
 
   def create
-    @attendees_event = Guestlist.create(event_id: params[:event_id], attendee_id: current_user.id, status: true)
+    @attendees_event = Guestlist.create(event_id: params[:event_id], attendee_id: current_user.id)
     flash[:notice] = "Your RSVP has been recorded."
     redirect_to event_path(params[:event_id])
   end
