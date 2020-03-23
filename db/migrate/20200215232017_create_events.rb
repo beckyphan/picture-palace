0ce4f3cb9caf604e4b494ad8edea4dad1c08a4f9
beckyphan <github email address>
2020-03-22 20:45:49 -0700

    noted host_id as foreign key; did not resolve build errors for user.event.build

--- db/migrate/20200215232017_create_events.rb
+++ db/migrate/20200215232017_create_events.rb
@@ -4,7 +4,7 @@ class CreateEvents < ActiveRecord::Migration[6.0]
       t.string :event_title
       t.datetime :date_time
       t.belongs_to :movie, null: false, foreign_key: true
-      t.integer :host_id, null: false
+      t.integer :host_id, null: false, foreign_key: true
 
       t.timestamps
     end
